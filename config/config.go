a0674e269664b692492661ea9f8d9e88eb5b2049
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-11-02 22:45:37 +0500

    logger and config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	DB_NAME string
+	DB_USER string
+	DB_PASS string
+	DB_HOST string
+	DB_PORT string
+}
+
+func NewConfig() *Config {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config := &Config{}
+
+	config.DB_NAME = os.Getenv("DB_NAME")
+	config.DB_USER = os.Getenv("DB_USER")
+	config.DB_PASS = os.Getenv("DB_PASS")
+	config.DB_NAME = os.Getenv("DB_NAME")
+	config.DB_PORT = os.Getenv("DB_PORT")
+
+	return config
+}
